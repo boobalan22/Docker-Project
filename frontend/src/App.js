7a3d5341fd1f790ded02b5058b6d378bb5f2ab15
BOOBALAN A <136874985+boobalan22@users.noreply.github.com>
2025-06-27 14:00:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/users')
+      .then(res => setUsers(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} ({user.email})</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
