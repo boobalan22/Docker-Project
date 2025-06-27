a1bb889893f774465fe1b616688dbc2f303dbe07
BOOBALAN A <136874985+boobalan22@users.noreply.github.com>
2025-06-27 14:05:39 +0530

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE IF NOT EXISTS myapp;
+
+USE myapp;
+
+CREATE TABLE IF NOT EXISTS user (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255)
+);
+
+INSERT INTO user (name, email) VALUES ('Alice', 'alice@example.com'), ('Bob', 'bob@example.com');
 
